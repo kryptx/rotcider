96cda3e51a91b3d41937187b6feb43e6ea759290
Jason Burbage <kryptx@gmail.com>
2018-03-31 11:36:41 -0600

    Abbreviate a couple of long lines

--- src/rpc-over-http.test.js
+++ src/rpc-over-http.test.js
@@ -34,11 +34,12 @@ describe('rpc-over-http module', () => {
     it('should use the state marshal to decode cookies from request', () => {
       stubs.req.cookies = sampleState;
       RpcOverHttp.readState(stubs)(stubs.req, stubs.res, () => {
-        Assert.equal(stubs.StateMarshal.decode.callCount, 3);
-        Assert.deepEqual(stubs.StateMarshal.decode.firstCall.args, [ stubs.req.cookies.one ]);
-        Assert.deepEqual(stubs.StateMarshal.decode.secondCall.args, [ stubs.req.cookies.two ]);
-        Assert.deepEqual(stubs.StateMarshal.decode.thirdCall.args, [ stubs.req.cookies.three ]);
-        Assert.deepEqual(stubs.res.locals.state, stubs.req.cookies);
+        let decode = stubs.StateMarshal.decode;
+        Assert.equal(decode.callCount, 3);
+        Assert.deepEqual(decode.firstCall.args, [ sampleState.one ]);
+        Assert.deepEqual(decode.secondCall.args, [ sampleState.two ]);
+        Assert.deepEqual(decode.thirdCall.args, [ sampleState.three ]);
+        Assert.deepEqual(stubs.res.locals.state, sampleState);
       });
     });
   });
@@ -47,12 +48,14 @@ describe('rpc-over-http module', () => {
     it('should use the state marshal to encode cookies and send them', () => {
       stubs.res.locals.state = sampleState;
       RpcOverHttp.writeState(stubs)(stubs.req, stubs.res, () => {
-        Assert.deepEqual(stubs.StateMarshal.encode.firstCall.args, [ stubs.res.locals.state.one ]);
-        Assert.deepEqual(stubs.StateMarshal.encode.secondCall.args, [ stubs.res.locals.state.two ]);
-        Assert.deepEqual(stubs.StateMarshal.encode.thirdCall.args, [ stubs.res.locals.state.three ]);
-        Assert.deepEqual(stubs.res.cookie.firstCall.args, ['one', stubs.res.locals.state.one ]);
-        Assert.deepEqual(stubs.res.cookie.secondCall.args, ['two', stubs.res.locals.state.two ]);
-        Assert.deepEqual(stubs.res.cookie.thirdCall.args, ['three', stubs.res.locals.state.three ]);
+        let encode = stubs.StateMarshal.encode;
+        let cookies = stubs.res.cookie;
+        Assert.deepEqual(encode.firstCall.args, [ sampleState.one ]);
+        Assert.deepEqual(encode.secondCall.args, [ sampleState.two ]);
+        Assert.deepEqual(encode.thirdCall.args, [ sampleState.three ]);
+        Assert.deepEqual(cookies.firstCall.args, ['one', sampleState.one ]);
+        Assert.deepEqual(cookies.secondCall.args, ['two', sampleState.two ]);
+        Assert.deepEqual(cookies.thirdCall.args, ['three', sampleState.three ]);
       });
     });
   });
